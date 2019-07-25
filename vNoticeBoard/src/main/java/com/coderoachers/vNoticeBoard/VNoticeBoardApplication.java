791d31876e7ad542b23c2396f3f4c608f139d1df
shahsami <shah.sami77@gmail.com>
2019-07-26 03:43:33 +0600

    initial commit

---
+++ vNoticeBoard/src/main/java/com/coderoachers/vNoticeBoard/VNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.coderoachers.vNoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VNoticeBoardApplication.class, args);
+	}
+
+}
